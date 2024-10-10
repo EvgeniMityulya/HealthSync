389d865bf41da09fabc7fed8873525cd6dd2d5a5
iseedeadpeople <evgenni2004@gmail.com>
2024-10-10 15:02:08 +0300

    INIT
    Initialize main project

---
+++ HealthSync/HealthSync/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HealthSync
+//
+//  Created by Евгений Митюля on 10/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        EmptyView()
+            .ignoresSafeArea()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ HealthSync/HealthSync/HealthSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthSyncApp.swift
+//  HealthSync
+//
+//  Created by Евгений Митюля on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
